6685a1b6d67d4d94ae55d549488530c29843fba8
tannvi22420316-cloud <tannvi.22420316@viit.ac.in>
2026-04-17 18:23:18 +0530

    Add login path to Django app URLs

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,7 +10,8 @@ urlpatterns = [
 
     # path for login
     # path(route='login', view=views.login_user, name='login'),
-
+    path(route='login', view=views.login_user, name='login'),
+    path('login/', TemplateView.as_view(template_name="index.html")),
     # path for dealer reviews view
 
     # path for add a review view
